913c98453c7a509f6484f829d2c4e605994b2b53
JustCallMeSimon <github@jcms.dev>
2023-12-26 17:00:59 +0100

    final v1

--- index.js
+++ index.js
@@ -2,52 +2,80 @@ document.addEventListener('DOMContentLoaded', function () {
 	// Get the container and buttons
 	const container = document.querySelector('.mainContainer');
 	const addButton = document.querySelector('.addButton');
-  
-	// Load entries from localStorage on page load
-	loadEntries();
-  
+    
+	loadEntries()
+	
 	// Add event listener for the "+" button
 	addButton.addEventListener('click', function () {
 	  addNewEntry();
-	  saveEntries();
 	});
-  
-	// Add event listener for the "-" buttons
-	container.addEventListener('click', function (event) {
-	  if (event.target && event.target.className === 'ctmButton') {
-		// Remove the entry when "-" button is clicked
-		const entryToRemove = event.target.parentNode;
-		container.removeChild(entryToRemove);
-		saveEntries();
-	  }
-	});
-  
+    
 	// Add event listener for "Enter" key press in text input
 	container.addEventListener('keyup', function (event) {
 	  if (event.target && event.target.className === 'ctmText' && event.key === 'Enter') {
 		addNewEntryAfter(event.target.parentNode);
-		saveEntries();
 	  }
 	});
-  
+	
+	container.addEventListener('keyup', function (event) {
+	  if (event.target && event.target.className === 'ctmText' && event.key != 'Enter') {
+		saveEntries()
+	  }
+	});
+
+	// ---
+	
+	
+	function saveEntries() {
+		// generate list
+		entries = new Array()
+		entryElements = container.querySelectorAll(".entry")
+		for (let index = 0; index < entryElements.length; index++) {
+			const entry = entryElements[index];
+			text = entry.querySelector(".ctmText").value
+			checked = entry.querySelector(".ctmCheckbox").checked
+			entries.push([text,checked])
+		}
+		for (let index = 0; index < entries.length; index++) {
+			const text = entries[index][0];
+			if (text == "") {
+				entries.splice(index,1)
+			}
+		}
+		console.log(entries)
+		localStorage.setItem("jcmsTODO",JSON.stringify(entries))
+	}	
+	
+	function loadEntries() {
+		rawData = localStorage.getItem("jcmsTODO")
+		entries = JSON.parse(rawData)
+		console.log(entries)
+		for (let index = 0; index < entries.length; index++) {
+			const text = entries[index][0];
+			const checked = entries[index][1];
+			entry = addNewEntry()
+			entry.querySelector(".ctmText").value = text
+			entry.querySelector(".ctmCheckbox").checked = checked
+		}
+	}
+	
+	// ---
+	
 	function addNewEntry() {
 	  // Create a new entry
-	  const newEntry = createEntry();
-  
+	  const newEntry = createEntry();  
 	  // Append the new entry to the container
 	  container.insertBefore(newEntry, addButton);
-  
 	  // Focus on the new text input
 	  newEntry.querySelector('.ctmText').focus();
+	  return newEntry
 	}
   
 	function addNewEntryAfter(previousEntry) {
 	  // Create a new entry
 	  const newEntry = createEntry();
-  
 	  // Insert the new entry after the previous entry
 	  container.insertBefore(newEntry, previousEntry.nextSibling);
-  
 	  // Focus on the new text input
 	  newEntry.querySelector('.ctmText').focus();
 	}
@@ -56,17 +84,17 @@ document.addEventListener('DOMContentLoaded', function () {
 	  // Create a new entry
 	  const newEntry = document.createElement('div');
 	  newEntry.className = 'entry';
-  
 	  // Create checkbox
 	  const checkbox = document.createElement('input');
 	  checkbox.type = 'checkbox';
-	  checkbox.className = 'ctmCheckbox';
-  
+	  checkbox.className = 'ctmCheckbox';  
+	  checkbox.addEventListener('click', function () {
+		saveEntries();
+	  });
 	  // Create text input
 	  const textInput = document.createElement('input');
 	  textInput.type = 'text';
 	  textInput.className = 'ctmText';
-  
 	  // Create "-" button for the new entry
 	  const removeButton = document.createElement('button');
 	  removeButton.className = 'ctmButton';
@@ -75,40 +103,11 @@ document.addEventListener('DOMContentLoaded', function () {
 		container.removeChild(newEntry);
 		saveEntries();
 	  });
-  
 	  // Append elements to the new entry
 	  newEntry.appendChild(checkbox);
 	  newEntry.appendChild(textInput);
 	  newEntry.appendChild(removeButton);
-  
 	  return newEntry;
-	}
-  
-	function saveEntries() {
-	  // Save entries to localStorage
-	  const entries = Array.from(container.children).map(entry => ({
-		checked: entry.querySelector('.ctmCheckbox').checked,
-		text: entry.querySelector('.ctmText').value
-	  }));
-  
-	  localStorage.setItem('todo_entries', JSON.stringify(entries));
-	}
-  
-	function loadEntries() {
-	  // Load entries from localStorage
-	  const savedEntries = localStorage.getItem('todo_entries');
-  
-	  if (savedEntries) {
-		const entries = JSON.parse(savedEntries);
-  
-		entries.forEach(entry => {
-		  const newEntry = createEntry();
-		  newEntry.querySelector('.ctmCheckbox').checked = entry.checked;
-		  newEntry.querySelector('.ctmText').value = entry.text;
-  
-		  container.appendChild(newEntry);
-		});
-	  }
 	}
   });
   
